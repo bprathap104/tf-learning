627991d579f7d516ef9b5c03a8cc92a155110ea6
Saravanan <bprathap104@gmail.com>
2025-03-03 14:49:46 +0530

    Testing jenkins

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "webserver1" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-09014e6e8df51b4f3"
-  vpc_security_group_ids  = ["sg-0a8a6d2c64748eaf8"]
+  subnet_id     = "subnet-0e5c278c40c11a698"
+  vpc_security_group_ids  = ["sg-024a2baec38f6fc72"]
   tags = {
     Name = "Webserver1"
   }
